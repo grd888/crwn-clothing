40482d2500a5fe56d2a2286ef229b74e6c07d141
Greg Delgado <grd888@gmail.com>
2024-09-19 21:33:34 +0800

    Use Outlet to display a Navigation Bar with the Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
 
 const App = () => {
   return <Routes>
-    <Route path="/home" element={<Home />} />
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />} />
+    </Route>
   </Routes>;
 };
 
